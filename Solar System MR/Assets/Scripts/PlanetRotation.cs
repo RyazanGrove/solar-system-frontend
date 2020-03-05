1e12c424c9d805947a9dc44acc008b10fc47b302
Mikhail <m.raskhodchikov@accenture.com>
2020-03-05 15:58:58 +0300

    clickable one planet with rotation

---
+++ Solar System MR/Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centralPoint;
+
+    [SerializeField]
+    [Range(0.1f,50f)]
+    private float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(centralPoint.position, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
