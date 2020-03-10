f09721585fd6d50cdddaa3948281cf2603be600d
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-03-10 10:54:12 +0300

    ShowPlanetName script has been added

---
+++ Solar System MR/Assets/Scripts/ShowPlanetName.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPlanetName : MonoBehaviour
+{
+    [SerializeField]
+    public string planetName;
+
+    [SerializeField]
+    [Range(0f,2f)]
+    public float upDistance;
+
+    [SerializeField]
+    public Transform cameraTransform;
+
+    private TextMesh textMesh;
+    private GameObject textObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textObject = new GameObject();
+        textObject.transform.parent = gameObject.transform;
+        textObject.transform.position = gameObject.transform.position;
+        textObject.transform.Translate(new Vector3(0f,upDistance,0f));
+        textObject.transform.Rotate(90f,0f,180f);
+        textMesh = textObject.AddComponent<TextMesh>();
+        textMesh.text = planetName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textObject.transform.LookAt(cameraTransform);
+        textObject.transform.Rotate(0f,-180f,0f);
+    }
+}
