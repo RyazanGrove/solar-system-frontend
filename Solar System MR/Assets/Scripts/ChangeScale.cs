a78cf8ed16a12335eda838012b11a5693e582fe1
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-03-13 23:52:10 +0300

    ChangeScale script has been added

---
+++ Solar System MR/Assets/Scripts/ChangeScale.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    [SerializeField]
+    private bool increase;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    [Range(0.1f,2f)]
+    private float value;
+
+    public void OnSelect()
+    {
+        if(target != null)
+        {
+            Vector3 temp = target.transform.localScale;
+            if (temp.x > 0.2f && temp.x < 2.3f)
+            {
+                target.transform.localScale += increase ? new Vector3(value, value, value) : new Vector3(-value, -value, -value);
+            }
+            else
+            {
+                if(temp.x <= 0.2f && increase)
+                {
+                    target.transform.localScale += new Vector3(value, value, value);
+                }
+                if(temp.x >= 2.3f && !increase)
+                {
+                    target.transform.localScale += new Vector3(-value, -value, -value);
+                }
+            }
+
+        }
+    }
+}
+
