ed905df19439a50d38d91b339962844049beb6ea
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-03-13 16:28:34 +0300

    moon rotation stuff and two prefabs with moon have been added

---
+++ Solar System MR/Assets/Scripts/MoonRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotation : MonoBehaviour
+{
+    [SerializeField]
+    public Transform centralPoint;
+
+    [SerializeField]
+    [Range(0.1f, 50f)]
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (centralPoint == null)
+        {
+            Debug.LogWarning("centralPoint is not assing onto object " + gameObject.name + ". Default value will be used");
+            centralPoint = gameObject.transform.parent != null ? gameObject.transform.parent : gameObject.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(centralPoint.position, new Vector3(1f,1f,0f), rotationSpeed * Time.deltaTime);
+    }
+}
