c131752a4e74ff3f8f02783dd3b0d4f30933bab4
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-03-15 16:57:26 +0300

    ReturnToUniverseViewScene script has been added

---
+++ Solar System MR/Assets/Scripts/ReturnToUniverseViewScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToUniverseViewScene : MonoBehaviour
+{
+    public void OnSelect()
+    {
+        SceneManager.LoadScene("UniverseView");
+    }
+}
