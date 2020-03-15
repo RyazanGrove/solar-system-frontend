99bb27b87add89fb0d5a2c25dcb3ef1f54ae3d8e
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-03-15 23:28:27 +0300

    Inforgraphy for SinglePlanetScene has been added. !But there is bug in scale transformation

---
+++ Solar System MR/Assets/Scripts/LoadInfography.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadInfography : MonoBehaviour
+{
+    //    [SerializeField] private UnityEngine.UI.Image image = null;
+    private GameObject plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+        plane.transform.localScale = new Vector3(0.7f, 1f, 1f);
+        
+        plane.transform.position = Vector3.zero;
+        plane.transform.parent = gameObject.transform;
+        plane.transform.Rotate(90, -180, -180);
+        Renderer rend = plane.GetComponent<Renderer>();
+        rend.material.mainTexture = Resources.Load("3/1") as Texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
