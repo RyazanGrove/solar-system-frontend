a112d8b72d08f3806de47b8277d4571872b21af8
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-03-16 17:19:36 +0300

    Information about earth has been added. Functions for changing slides have been added

--- Solar System MR/Assets/Scripts/LoadInfography.cs
+++ Solar System MR/Assets/Scripts/LoadInfography.cs
@@ -6,24 +6,46 @@ public class LoadInfography : MonoBehaviour
 {
     //    [SerializeField] private UnityEngine.UI.Image image = null;
     private GameObject plane;
+    private int currentPage;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
 
-        plane.transform.localScale = new Vector3(0.7f, 1f, 1f);
-        
-        plane.transform.position = Vector3.zero;
         plane.transform.parent = gameObject.transform;
+        plane.transform.position = new Vector3(1.5f, 0f, 1.5f);
+        plane.transform.localScale = new Vector3(0.15f, 0.2f, 0.2f);
         plane.transform.Rotate(90, -180, -180);
+
         Renderer rend = plane.GetComponent<Renderer>();
-        rend.material.mainTexture = Resources.Load("3/1") as Texture;
+        currentPage = 1;
+        rend.material.mainTexture = Resources.Load("3/" + currentPage.ToString()) as Texture;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShowNextPicture()
+    {
+        if (currentPage < 4)
+        {
+            currentPage++;
+            Renderer rend = plane.GetComponent<Renderer>();
+            rend.material.mainTexture = Resources.Load("3/" + currentPage.ToString()) as Texture;
+        }
+    }
+
+    public void ShowPreviousPicture()
+    {
+        if (currentPage >1)
+        {
+            currentPage--;
+            Renderer rend = plane.GetComponent<Renderer>();
+            rend.material.mainTexture = Resources.Load("3/" + currentPage.ToString()) as Texture;
+        }
     }
 }
