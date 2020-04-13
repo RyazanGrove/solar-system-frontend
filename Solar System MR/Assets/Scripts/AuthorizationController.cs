d6849eb8ee1fa3149f8d7a1218a6f0eb6e328ef4
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-04-13 23:05:50 +0300

    authorization has been implemented

--- Solar System MR/Assets/Scripts/AuthorizationController.cs
+++ Solar System MR/Assets/Scripts/AuthorizationController.cs
@@ -19,17 +19,13 @@ public class AuthorizationController : MonoBehaviour
     private Text loginButtonText;
     [SerializeField]
     private Text passwordButtonText;
+    [SerializeField]
+    private string loginURL = "http://localhost/unity_login.php";
 
     // Start is called before the first frame update
     void Start()
     {
-        //keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false, keyboardText);
-        /*    InputField inputField = gameObject.AddComponent<InputField>(); //<InputField>();//InputTextBox
-            inputField.targetGraphic = null; // = inputField;
-            */
 
-        // Single-line password box with title
-        //keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true, false, "Secure Single-line Title");
     }
 
     // Update is called once per frame
@@ -76,4 +72,55 @@ public class AuthorizationController : MonoBehaviour
         PlayerPrefs.SetInt("isAuthorized", 0);
         SceneManager.LoadScene("UniverseView");
     }
+
+    public void LoginWithAuthorization()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", userLogin);
+        form.AddField("password", userPassword);
+        WWW www = new WWW(loginURL, form);
+        StartCoroutine(LoginFunc(www));
+    }
+
+	IEnumerator LoginFunc(WWW www)
+	{
+		yield return www;
+
+		if (www.error == null)
+		{
+            AuthorizationParser answerWWW = AuthorizationParser.CreateFromJSON(www.text);
+         
+            if (!answerWWW.error)
+            {
+                
+                PlayerPrefs.SetInt("id_user", answerWWW.id_user);
+                PlayerPrefs.SetInt("planetNumber", answerWWW.last_scene);
+                for (int i=0; i<10; i++)
+                {
+                    PlayerPrefs.SetString("planetProgress"+i, answerWWW.planetProgress[i]);
+                }
+               
+                if (answerWWW.last_scene == 10)
+                {
+                    PlayerPrefs.SetInt("isAuthorized", 1);
+                    SceneManager.LoadScene("UniverseView", LoadSceneMode.Single);
+                }
+                else
+                {
+                    PlayerPrefs.SetInt("isAuthorized", 1);
+                    SceneManager.LoadScene("SinglePlanetScene", LoadSceneMode.Single);
+                }
+            }
+            else
+            {
+                Debug.Log(answerWWW.errorMessage);
+            }
+		
+			
+		}
+		else
+		{
+			Debug.Log("Error: " + www.error);
+		}
+	}
 }
---
+++ Solar System MR/Assets/Scripts/AuthorizationParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AuthorizationParser
+{
+    public int id_user;
+    public bool error;
+    public int last_scene;
+    public string errorMessage;
+    public string[] planetProgress;
+
+
+    public static AuthorizationParser CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<AuthorizationParser>(jsonString);
+    }
+
+}
+
