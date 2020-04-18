22d57d22ae73f0530b733fab3c9a8a71cb25a735
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-04-18 18:33:26 +0300

    AsteroidRotation script has been added

---
+++ Solar System MR/Assets/Scripts/AsteroidRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidRotation : MonoBehaviour
+{
+    private Vector3 rotationSpeed;
+    [SerializeField]
+    [Range(0f,10f)]
+    private float maxRandomNuber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = new Vector3(Random.Range(0f, maxRandomNuber), Random.Range(0f, maxRandomNuber), Random.Range(0f, maxRandomNuber));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed);
+    }
+}
