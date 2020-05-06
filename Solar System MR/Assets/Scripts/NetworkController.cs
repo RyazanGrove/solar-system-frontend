5d59e95f1268af3daaca4ebce012e0174c515e42
Mikhail R <RyazanGrove@users.noreply.github.com>
2020-05-06 21:01:25 +0300

    NetworkController and PlayerController scripts have been added

---
+++ Solar System MR/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkController : NetworkBehaviour
+{
+    [SerializeField]
+    private string networkAddress;
+
+    [SerializeField]
+    private Button changeScaleButton;
+
+    NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+        Debug.Log(networkManager.networkAddress);
+        Debug.Log(networkManager.networkPort);
+
+        if (networkAddress.Equals("") || networkAddress.Equals("localhost"))
+        {
+            StartHost();
+        } 
+        else
+        {
+            networkManager.networkAddress = networkAddress;
+            StartClient();
+        }
+    }
+    public void StartHost()
+    {
+        Debug.Log("Starting host");
+        networkManager.StartHost(); 
+        Debug.Log("Started host on " + networkManager.networkAddress + " and port " + networkManager.networkPort);
+    }
+    public void StartClient()
+    {
+        Debug.Log("Starting client");
+        //TODO: make good check for connection of client
+        NetworkClient client = networkManager.StartClient();
+        if (client != null)
+        {
+            Debug.Log("Started client on " + networkManager.networkAddress + " and port " + networkManager.networkPort);
+        }
+        else
+        {
+            Debug.LogWarning("Client has not connected to " + networkManager.networkAddress + " and port " + networkManager.networkPort);
+        }
+    }
+    public void OnDestroy()
+    {
+        networkManager.StopClient();
+        networkManager.StopHost();
+    }
+}
---
+++ Solar System MR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerController : NetworkBehaviour
+{
+    private Button increaseButton;
+    private Button decreaseButton;
+
+    private ExperimentController experimentController;
+
+    // Start is called before the first frame update
+
+    public void IncreaseAsteroidSize()
+    {
+        CmdIncreaseAsteroidSize();
+    }
+
+    [Command]
+    public void CmdIncreaseAsteroidSize()
+    {
+        RpcIncreaseAsteroidSize();
+    }
+
+    [ClientRpc]
+    public void RpcIncreaseAsteroidSize()
+    {
+        experimentController.IncreaseAsteroidSize();
+    }
+
+    
+    public void DecreaseAsteroidSize()
+    {
+        CmdDecreaseAsteroidSize();
+    }
+
+    [Command]
+    public void CmdDecreaseAsteroidSize()
+    {
+        RpcDecreaseAsteroidSize();
+    }
+
+    [ClientRpc]
+    public void RpcDecreaseAsteroidSize()
+    {
+        experimentController.DecreaseAsteroidSize();
+    }
+
+
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            experimentController = GameObject.FindGameObjectWithTag("ExperimentControl").GetComponent<ExperimentController>();
+
+            increaseButton = GameObject.FindGameObjectWithTag("IncreaseAsteroidSizeButton").GetComponent<Button>();
+            increaseButton.onClick.AddListener(() => { IncreaseAsteroidSize(); });
+            decreaseButton = GameObject.FindGameObjectWithTag("DecreaseAsteroidSizeButton").GetComponent<Button>();
+            decreaseButton.onClick.AddListener(() => { DecreaseAsteroidSize(); });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
